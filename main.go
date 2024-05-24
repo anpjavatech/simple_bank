73e893b6790ec3d6919a74d278648f96bd66aed4
Anoop Kuttikattu Sukumaran Nair <anoop.ks007@gmail.com>
2024-05-24 14:07:17 +0200

    commit after adding POST api to create an account.

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	"github.com/anpjavatech/simple_bank/api"
+	db "github.com/anpjavatech/simple_bank/db/sqlc"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://anoop:root@localhost:5433/simple-bank?sslmode=disable"
+	address  = "localhost:8080"
+)
 
 func main() {
-	fmt.Println("Hallo Anoop.")
+	conn, err := sql.Open(dbDriver, dbSource)
+
+	if err != nil {
+		log.Fatal("Cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(address)
+	if err != nil {
+		log.Fatal("cannot start server:", err.Error())
+	}
 }
